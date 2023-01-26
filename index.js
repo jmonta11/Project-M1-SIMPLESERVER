800d5bd355168553d8acdf65562daa9f779ef658
jmonta11 <115600410+jmonta11@users.noreply.github.com>
2023-01-25 21:54:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,68 +2,115 @@ const httpServer = require('http');
 const url = require('url');
 const fs = require('fs');
 
-const replaceTemplate = require('./modules/replaceTemplate')
+const replaceTemplate = require('modules/replaceTemplate');
 
-////Read data from file
-///Template
+
+/// Read data from file
+// Template
 const tempCourse = fs.readFileSync(
-    '${__dirname}/data.json',
-    'utf-8'
-    );
-    
-
- //   const dataObj = JSON.parse(tempCourse);//string to JavaScript Object JSON
-
-
- const templateHTMLCourse = fs.readFileSync(
-    '${__dirname}/template/templateCourse.html',
+    `${__dirname}/data/data.json`,
     'utf-8'
  );
 
+ /////////////////////////////////
+// Template
+const templateHTMLCourse = fs.readFileSync(
+    `${__dirname}/template/templateCourse.html`,
+    'utf-8'
+  );
+  interest = (amount * (rate * 0.01))/months;
+total = ((amount/months) + interest);
 
- //module.exports = (htmlStr, course)=>{ //fat arrow or lambda
- //let output = htmlStr.replace(/{%NAME%}/g, course.courseName);
- //output = output.replace(/{%IMAGE%}/g, course.image);
- // output = output.replace(/{%FROM%}/g, course.from);
-//output = output.replace(/{%INSTRUCTOR%}/g, course.instructor);
- //output = output.replace(/{%CREDITS%}/g, course.credit);
- //output = output.replace(/{%DESCRIPTION%}/g, course.description);
- //output = output.replace(/{%ID%}/g, course.id);
- // return output; 
-//}
- ////////////////////
+  function Calculate() {
+  
+    // Extracting value in the amount 
+    // section in the variable
+    const amount = document.querySelector("#amount").value;
+  
+    // Extracting value in the interest
+    // rate section in the variable
+    const rate = document.querySelector("#rate").value;
+  
+    // Extracting value in the months 
+    // section in the variable
+    const months = document.querySelector("#months").value;
+  
+    // Calculating interest per month
+    const interest = (amount * (rate * 0.01)) / months;
+      
+    // Calculating total payment
+    const total = ((amount / months) + interest).toFixed(2);
+  
+    document.querySelector("#total")
+        .innerHTML = "EMI : ($)" + total;
+  }
+// //   function replaceTemplate(htmlStr, course){
+// const replaceTemplate = (htmlStr, course)=>{ // fat arrow function or lambda
+//     let output = htmlStr.replace(/{%NAME%}/g, course.courseName);
+//     output = output.replace(/{%IMAGE%}/g, course.image);
+//     output = output.replace(/{%FROM%}/g, course.from);
+//     output = output.replace(/{%INSTRUCTOR%}/g, course.instructor);
+//     output = output.replace(/{%CREDITS%}/g, course.credits);
+//     output = output.replace(/{%DESCRIPTION%}/g, course.description);
+//     output = output.replace(/{%ID%}/g, course.id);
+//     return output;
+// }
+
+ const dataObj = JSON.parse(tempCourse);// string to JavaScript Object JSON
+
+////////////////////////////////
 //Create Server
-//const server = httpServer.createServer(function (req, res) {//call back function
-const server = httpServer.createServer( (req, res) =>{//call back function
+// const server = httpServer.createServer(function (req, res) {// call back function
+const server = httpServer.createServer( (req, res) =>{// call back function
 
-   // const urlParameter = url.parse(req.url, true);
-  //  console.log(JSON.stringify(urlParameter.query));//convert to string
-   // console.log(JSON.stringify(urlParameter.pathname));//convert to string
-    const {query,pathname} = url.parse(req.url, true); //object destructors 
-    if(urlParameter.query.id){// if there is query parameter named id read as string
-    //Courses page
-    if (pathname === '/' || pathname.toLowerCase() === '/courses') {
-        res.writeHead(200, {//everything ran successfully
-            'Content-type': 'text/html'
-        });
-        const course = dataObj[Number(query.id)];//convert string to numeric value
-        const StrCourseName = JSON.stringify(course);
-        const courseHTML = replaceTemplate(templateHTMLCourse, course);
-//function that will replace values in the HTML
-        // res.end('We received our first request from the client resource ${urlParameter.pathname.toLowerCase()} with query parameter ${urlParameter.query.id} ${course} ${JASON.sringify(course)}')
-    //convert object to string
-    res.end(courseHTML);
+    // const urlParameter = url.parse(req.url, true);
+    // console.log(JSON.stringify(urlParameter.query));// convert to String
+    // console.log(JSON.stringify(urlParameter.pathname));// convert to String
+    const {query,pathname} = url.parse(req.url, true); // object distructors
+    if(query.id){// if there is query parameter named id read as string
+        // Courses page
+        if (pathname === '/' || pathname.toLowerCase() === '/courses') {
+            res.writeHead(200, {// Every thing ran successfully
+                'Content-type': 'text/html'
+            });
+            const indexjs = require('indexjs')
+
+        // var loan = new indexjs.Loan(
+            //    1000, // amount
+            //    12, // installments
+            //    5, //Interest rate
+           //     true //diminishing
+          //  );
+
+          //  console.log(loan).JSON
+                
+         ///   const IR = dataObj[Number(query.id)].interestRate / 12;
+          //  const PMT = dataObj[Number(query.id)].monthlyPayment;
+          // const nMonths = dataObj[Number(query.id)].loanTermYears * 12;
+
+              // let mortgage = (interestRate * loanTermYears) /
+          //     (1 - Math.pow(1 + interestRate, -monthlyPayment));
+            /////////////////////////////////////////////
+
+            const course = dataObj[Number(query.id)];// convert string to numeric value
+            const strCourseName = JSON.stringify(course);
+            const courseHTML = replaceTemplate(templateHTMLCourse, course);// function that will replace the course values in the HTML
+            //   res.end(` We received our first request from the client at resource ${urlParameter.pathname.toLowerCase()} with query parameter ${urlParameter.query.id}
+            //   ${JSON.stringify(course)}// convert object back to string
+            //   `)
+            res.end(courseHTML);
+        }
     }
     else{
-        res.writeHead(404, {//server did not find what you were looking for
-            'Content-type': 'text/html'
-        });
-        res.end('resource not found')
-    }
-    }
-});
+            res.writeHead(404, {// Server did not find what you were looking for
+                'Content-type': 'text/html'
+            });
+            res.end(`resource not found`)
+        }
+    });
 
-//Start listening to requests
-server.listen(8000, 'localhost', function(){
+//Start Listening to requests
+server.listen(8000, 'localhost', ()=> {
     console.log('Listening to requests on port 8000');
 });
+
